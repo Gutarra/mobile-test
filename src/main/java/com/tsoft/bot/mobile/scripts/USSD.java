d6f9baae13b9c515b48f57f5670352d8997226fc
David Jonatan Gutarra Ramos <David.Gutarra@tsoftglobal.com>
2021-10-28 09:25:35 -0500

    set ussd test

--- src/main/java/com/tsoft/bot/mobile/scripts/USSD.java
+++ src/main/java/com/tsoft/bot/mobile/scripts/USSD.java
@@ -5,11 +5,8 @@ import com.tsoft.bot.mobile.helpers.HookMobile;
 import com.tsoft.bot.mobile.objects.MobileObjects;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
 
 public class USSD extends MobileBaseClass {
     private final AppiumDriver<MobileElement> driver = HookMobile.getDriver("MOBILE USSD");;
